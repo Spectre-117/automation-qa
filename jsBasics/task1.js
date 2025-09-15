f75bf09d256c05f0ac0e25bab7f796875a87f077
Serhii <lyalyuksn@gmail.com>
2025-09-16 00:16:36 +0300

    Task for lesson 4 is solved. Topic: datatypes and operators

---
+++ jsBasics/task1.js
@@ -0,0 +1,16 @@
+let name = "Serhii";
+let age = 34;
+let isTester = true;
+let personalProject = undefined;
+let asteroidsInSystem = 1234567890n;
+let highestMark = Symbol("A");
+let carObject = null;
+
+console.log('My name is ', name);
+console.log('I\'m ',age);
+console.log('My current job is tester: ', isTester);
+console.log(`Status of personal project: ${personalProject}`);
+console.log(`I\'ve sold my car. My current car: ${carObject}`);
+console.log('Solar system has such amount of asteroids: ', asteroidsInSystem);
+console.log('Highest mark at college is: ', highestMark.toString());
+
---
+++ jsBasics/task2.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+let nameOne = "Serhii";
+let nameTwo = "Anna";
+console.log(chalk.blue('Hello everyone. Please meet '+nameOne+' and '+nameTwo));
+console.log(chalk.red(`Hello everyone. Please meet ${nameOne} and ${nameTwo}`));
