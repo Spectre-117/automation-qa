7c272afcc62a404de5fedab4418a056dedd190ad
Serhii <lyalyuksn@gmail.com>
2025-09-23 01:31:58 +0300

    taskd with functions are implemented

---
+++ jsFunctions/task1.js
@@ -0,0 +1,15 @@
+function square(height, width) {
+    return height * width;
+}
+
+const square2 = function (height, width) {
+    return height * width;
+}
+
+const square3 = (height, width) => {
+    return height * width;
+}
+
+console.log(square(5,10));
+console.log(square2(5,10));
+console.log(square3(5,10));
