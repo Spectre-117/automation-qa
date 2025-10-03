2b627ca89dad33bb142dc8aca309e84bc2186f4b
Serhii <lyalyuksn@gmail.com>
2025-10-03 15:19:15 +0300

    tasks from Object topic are solved.

---
+++ jsObjects/task5.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        email: "jd@gmail.com",
+        age: 24
+    },
+    {
+        firstName: "Sam",
+        lastName: "Smith",
+        email: "ss@gmail.com",
+        age: 33
+    }
+];
+
+for (const {firstName, lastName, email, age} of users) {
+    console.log(`first name: ${firstName}, last name: ${lastName}, email: ${email}, age: ${age}`);
+}
