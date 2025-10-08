5b4ce168d0749ff7bf23a2fc8d0b813fd13ab2c9
Serhii <lyalyuksn@gmail.com>
2025-10-08 21:35:28 +0300

    task from oop lesson is implemented

---
+++ oopBasics/EBook.js
@@ -0,0 +1,27 @@
+import Book from "./Book.js";
+
+export default class EBook extends Book {
+    constructor(title, author, year, format) {
+        super(title, author, year);
+        this._format = format;
+    }
+
+    get format() {
+        return this._format;
+    }
+
+    set format(format) {
+        if (format === "") {
+            throw new Error("Format must be mentioned");
+        }
+        this._format = format;
+    }
+
+    printInfo() {
+        console.log("Book: " + this._title + ", Author: " + this._author + ", Year: " + this._year + ", format: " + this._format);
+    }
+
+    static transformBookIntoEBook(book, format) {
+        return new EBook(book.title, book.author, book.year, format);
+    }
+}
