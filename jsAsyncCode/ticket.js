ab14279a3f09d8c1f2c1e18505fa62a5c46c022d
Serhii <lyalyuksn@gmail.com>
2025-10-19 14:26:32 +0300

    training with promises are added

---
+++ jsAsyncCode/ticket.js
@@ -0,0 +1,18 @@
+export default class TicketManager {
+    #ticketsNumber;
+
+    constructor(ticketsNumber) {
+        this.#ticketsNumber = ticketsNumber;
+
+    }
+
+    isTicketsAvailable(ticketsNumber) {
+        if (ticketsNumber <= this.#ticketsNumber) {
+            const promiseA = Promise.resolve(true);
+            return promiseA;
+        } else {
+            const promiseV = Promise.resolve(false);
+            return promiseV;
+        }
+    }
+}
